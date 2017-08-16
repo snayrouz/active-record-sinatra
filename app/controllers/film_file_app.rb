d11bd53b5f577d0f4e49e268519483d723cfdaae
snayrouz <samuel.nayrouz@gmail.com>
2017-08-16 17:42:35 -0600

    Add director

--- app/controllers/film_file_app.rb
+++ app/controllers/film_file_app.rb
@@ -1,5 +1,12 @@
 class FilmFile < Sinatra::Base
+
+  get '/genres' do
+    @genres = Genre.all
+    erb :genres_index
+  end
+
   get '/films' do
+    @films = Film.all
     erb :films_index
   end
 end
